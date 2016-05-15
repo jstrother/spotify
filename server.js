d256df2519ec5c3572bec9cee69695157bba7d31
Jim Strother <jim.strother@gmail.com>
2016-05-14 18:57:11 -0700

    trying to fetch related artists, something isn't right

--- server.js
+++ server.js
@@ -15,11 +15,25 @@ var getFromApi = function(endpoint, args) {
 				 });
 	return emitter;
 };
+var getRelatedFromApi = function(id, args) {
+	var emitterRel = new events.EventEmitter();
+	unirest.get('https://api.spotify.com/v1/' + id + '/related-artists')
+				 .qs(args)
+				 .end(function(response) {
+					 	if (response.ok) {
+					 		emitterRel.emit('end', response.body);
+					 	} else {
+					 		emitterRel.emit('error', response.code);
+					 	}
+				 });
+	return emitterRel;
+};
 
 var app = express();
 app.use(express.static('public'));
 
 app.get('/search/:name', function(req, res) {
+	var id = null;
 	var searchReq = getFromApi('search', {
 		q: req.params.name,
 		limit: 1,
@@ -27,7 +41,13 @@ app.get('/search/:name', function(req, res) {
 	});
 	searchReq.on('end', function(item) {
 		var artist = item.artists.items[0];
+		id = item.artists.items[0].id;
 		res.json(artist);
+	});
+	var searchRel = getRelatedFromApi('id', {
+		q: req.params.id,
+		limit: 5,
+		type: 'artist'
 	});
 	searchReq.on('error', function(code) {
 		res.sendStatus(code);
